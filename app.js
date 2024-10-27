08eefd833ade018166a7957b320840485490a4f0
Loay Al-Mugharghar <almughargharloay@gmail.com>
2024-10-27 20:45:26 +0300

    init Lab + Ignore

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const express = require("express")
 const app = express()
+
+const prompt = require("prompt-sync")()
+
+const username = prompt("What is your name? ")
+
+console.log(`Your name is ${username}`)
